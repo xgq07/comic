7cd9933479dc0ea66a171d6b4686fce45c0b5a6d
qiu <goodgod2006@126.com>
2020-05-15 09:36:13 +0800

    abc

--- comic.py
+++ comic.py
@@ -22,12 +22,13 @@ headers = {'Accept': 'text/html, application/xhtml+xml, image/jxr, */*',
                "Pramgma":"public",
                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063'}
 
+session = requests.Session()
 
 def saveImage(index, url, path, book_title):
     i = 0
     while i < 3: 
         try:
-            response = requests.get(url, headers=headers, timeout=8)
+            response = session.get(url, headers=headers, timeout=8)
             image = Image.open(BytesIO(response.content))
             image_name = f'{path}\{index}.jpg'
             image.save(image_name)
@@ -163,7 +164,7 @@ def isDownloaded(doc, path):
         return True
 
 def getResponse(url):
-    resp = requests.get(url=url, headers=headers, timeout=8)
+    resp = session.get(url=url, headers=headers, timeout=8)
     html = resp.text
     doc = pq(html)  # 解析html字符串
     return doc
@@ -184,4 +185,4 @@ def main(url):
 
 
 if __name__ == "__main__":
-    main("https://www.manhuadb.com/manhua/17662")
+    main("https://www.manhuadb.com/manhua/12850")
